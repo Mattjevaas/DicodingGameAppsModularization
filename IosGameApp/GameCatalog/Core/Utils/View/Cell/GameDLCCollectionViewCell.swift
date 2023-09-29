258366e0416f3fe41b3eb3bae8d441a510698fb0
Mattjevaas <johaneswiku@protonmail.ch>
2023-09-29 14:27:48 +0700

    feat: add Game DLC Module

---
+++ IosGameApp/GameCatalog/Core/Utils/View/Cell/GameDLCCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GameDLCCollectionViewCell.swift
+//  GameCatalog
+//
+//  Created by admin on 28/09/23.
+//
+
+import UIKit
+
+class GameDLCCollectionViewCell: UICollectionViewCell {
+    
+}
